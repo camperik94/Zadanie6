71859c572c05473f0a0f1c0110ecac38dcff8632
camperik94 <tomaszjach94@gmail.com>
2017-12-24 22:51:37 +0100

    Funkcjonalnosc 0.1

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
+		System.out.println("To jest kalkulator");
 		
+		Scanner in = new Scanner(System.in);
+		
+		double liczba1 = in.nextDouble();
+		char zn = in.next().charAt(0);
+		double liczba2 = in.nextDouble();
+
+		Kalkulator calc = new Kalkulator(liczba1, liczba2, zn);
+		
+		switch(zn) {
+			case '+':
+				calc.funkcja();
+				break;
+			case '-':
+				calc.funkcja();
+				break;
+			case '*':
+				calc.funkcja();
+				break;
+			case '/':
+				calc.funkcja();
+				break;
+		}
+			
 	}
 }
